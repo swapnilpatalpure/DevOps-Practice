dbcf84de20628c14be9a4bb5113ca32562cc92a3
Alienware <Alienware@Swap>
2022-06-17 20:20:59 +0530

    Hello world get mapping working condition

--- src/main/java/com/helloworld/controller/HelloController.java
+++ src/main/java/com/helloworld/controller/HelloController.java
@@ -1,9 +1,17 @@
 package com.helloworld.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
 	
-	public void helloWorld() {
+	@GetMapping(value = "/hello")
+	public ResponseEntity<String> helloWorld() {
 		System.out.println("Hello World");
+		return new ResponseEntity<String>("Hello World", HttpStatus.OK);
 	}
 
 }
